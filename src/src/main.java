2a91ba9ceefdc10298d70eedeb2140400441e698
Eren Sefer <esefer@student.tgm.ac.at>
2016-04-01 11:30:32 +0200

    added lib

--- src/src/main.java
+++ src/src/main.java
@@ -3,7 +3,7 @@ package src;
 public class main {
 
 	public static void main(String[] args) {
-		DBConnector conn = new DBConnector("192.168.157.128","postgres","postgres","1234");
+		DBConnector conn = new DBConnector("192.168.133.135","schokofabrik","schokouser","schokouser");
 	}
 
 }
