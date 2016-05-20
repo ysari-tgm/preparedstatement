66b846b48c2b4235af84f68f2d145b6bdca8399c
Yunus Sari <ysari@student.tgm.ac.at>
2016-05-20 17:30:11 +0200

    modified CLIParser.java

--- src/src/CLIParser.java
+++ src/src/CLIParser.java
@@ -1,5 +1,33 @@
 package src;
 
-public class CLIParser {
+import com.beust.jcommander.JCommander;
 
+/**
+ * In dieser Klasse werden die Daten abgefragt, den Werten für die Verbindung
+ * mit der Datenbank zugewiesen und die Verbindung aufgebaut.
+ * 
+ * @author Eren Sefer, Yunus Sari
+ * @version 2016-05-20
+ */
+public class CLIParser {
+	private String host;
+	private int port;
+	private String database;
+	private String user;
+	private String password;
+
+	public CLIParser(String input[]) {
+		port = 0;
+		Settings settings = new Settings();
+		JCommander cmd = new JCommander(settings, input);
+		String host = settings.getHost();
+		if (port == 0) {
+			port = 5432;
+		}
+		database = settings.getDatabase();
+		user = settings.getUser();
+		password = settings.getPassword();
+		
+		DBConnector conn = new DBConnector(host,port,user,user,password);
+	}
 }
