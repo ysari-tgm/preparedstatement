376b065550bd20d7c51aa567a04414b6df27c762
Yunus Sari <ysari@student.tgm.ac.at>
2016-05-20 17:14:37 +0200

    modified Settings.java

--- src/src/Settings.java
+++ src/src/Settings.java
@@ -5,11 +5,51 @@ import com.beust.jcommander.Parameter;
 public class Settings {
 	@Parameter(names = "-h", description = "Host", required = true)
 	private String host;
-	
+
+	@Parameter(names = "-p", description = "Port", required = false)
+	private String port;
+
+	@Parameter(names = "-d", description = "Host", required = true)
+	private String database;
+
+	@Parameter(names = "-u", description = "Host", required = true)
+	private String user;
+
+	@Parameter(names = "-pw", description = "Host", required = true)
+	private String password;
+
 	/**
-	 * @return	host
+	 * @return host
 	 */
-	public String getHostname() {
+	public String getHost() {
 		return host;
+	}
+
+	/**
+	 * @return port
+	 */
+	public String getPort() {
+		return port;
+	}
+
+	/**
+	 * @return database
+	 */
+	public String getDatabase() {
+		return database;
+	}
+
+	/**
+	 * @return user
+	 */
+	public String getUser() {
+		return user;
+	}
+
+	/**
+	 * @return password
+	 */
+	public String getPassword() {
+		return password;
 	}
 }
